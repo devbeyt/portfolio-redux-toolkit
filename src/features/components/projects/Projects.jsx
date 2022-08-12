da775bba5f1eab7102a1b4a97397a35f06983583
= <=>
2022-08-12 18:05:08 +0400

    8 12 2022

--- src/features/components/projects/Projects.jsx
+++ src/features/components/projects/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Projects() {
   return (
-    <section className='projects'>Projects</section>
+    <section className='projects' id="#projects">Projects</section>
   )
 }
 
