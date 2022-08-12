3e1aafabfa137332fb58218335d4dfcb719577ed
= <=>
2022-08-12 14:23:28 +0400

    8 12 2022

--- src/features/components/nav/navigationData.js
+++ src/features/components/nav/navigationData.js
@@ -2,9 +2,9 @@ export const navigationData = {
     ids:["id1","id2","id3","id4","id5"],
     entities:{
         id1:{id:"id1",name:"Home",link:"/"},
-        id2:{id:"id2",name:"Home",link:"/"},
-        id3:{id:"id3",name:"Home",link:"/"},
-        id4:{id:"id4",name:"Home",link:"/"},
-        id5:{id:"id5",name:"Home",link:"/"},
+        id2:{id:"id2",name:"About",link:"/"},
+        id3:{id:"id3",name:"Projects",link:"/"},
+        id4:{id:"id4",name:"Skills",link:"/"},
+        id5:{id:"id5",name:"Contact",link:"/"},
     }
 }
