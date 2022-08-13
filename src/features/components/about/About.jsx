83a558ed850ca13fed000bf64d258811a0fb78cb
= <=>
2022-08-13 19:18:52 +0400

    updated view section

--- src/features/components/about/About.jsx
+++ src/features/components/about/About.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 function About() {
-  return (<section>
-     About
+  return (<section className='about'>
+About
   </section>
   )
 }
--- src/features/components/view/View.jsx
+++ src/features/components/view/View.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import './../../styles/components.css'
 
 function View() {
-    return (
-        <div className="view">
-            <div className="view_static_txt">I'm</div>
+    return (<section class="view container">
+            <div className="view_text_container">
+            <div className="view_static_txt">Hi I'm</div>
             <ul className="view_dynamic_txt">
-                <li><span className='view_name'>Rasim <span className='visible_searname'>Hesenov</span></span></li>
+                <li><span className='view_name'>Rasim Hesenov</span></li>
                 <li><span>a Web Developer</span></li>
             </ul>
-        </div>
+            </div>
+
+         <div className="view_gij_container"></div>
+   </section>
     )
 }
 
